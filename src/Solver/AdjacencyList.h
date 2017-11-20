6b0011349d800e8d2297ee67a111e2a96d0b560f
Patrick Dangla <patrick.dangla@ifsttar.fr>
2017-11-20 15:59:42 +0100

    Forget to extract AdjacencyList.h

---
+++ src/Solver/AdjacencyList.h
@@ -0,0 +1,20 @@
+#ifndef ADJACENCYLIST_H
+#define ADJACENCYLIST_H
+
+
+/* vacuous declarations and typedef names */
+struct AdjacencyList_s  ; typedef struct AdjacencyList_s AdjacencyList_t ;
+
+
+
+#define AdjacencyList_GetNbOfNeighbors(adj)       ((adj)->ndest)
+#define AdjacencyList_GetNeighbor(adj)            ((adj)->dest)
+
+
+
+struct AdjacencyList_s {      /* Format */
+  unsigned int  ndest ;       /* Nb of neighbors */
+  int* dest ;                 /* Neighbors */
+} ;
+
+#endif
